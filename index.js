4b8450b40b4e59811f29d1c61214c9067e2fc81f
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2023-03-25 02:42:47 +0530

    Added CORS

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const PORT = 8000 || process.env.PORT;
 
 // importing routes
 const caesarCypherRoutes = require("./routes/caesar-cyper");
 
-// using body-parser
+// using body-parser and cors
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/", caesarCypherRoutes);
 
